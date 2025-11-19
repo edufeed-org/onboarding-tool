a868a335d698453d5b2f60e83885330c95abf498
highperfocused <highperfocused@pm.me>
2025-11-19 21:08:11 +0100

    Fix base path in production mode for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' && process.env.GITHUB_ACTIONS ? '/mkstack-onboarding-tool/' : '/',
+  base: mode === 'production' && process.env.GITHUB_ACTIONS ? '/onboarding-tool/' : '/',
   server: {
     host: "::",
     port: 8080,
